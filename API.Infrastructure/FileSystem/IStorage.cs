eb2cd1fe0eb37bb8088e45dc0b0923d2334d00b2
Вячеслав <Vyacheslav>
2022-09-04 18:10:27 +0300

    Реализовал метод добавления файла в файловую систему

---
+++ API.Infrastructure/FileSystem/IStorage.cs
@@ -0,0 +1,23 @@
+﻿using API.Infrastructure.FileSystem.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.FileSystem
+{
+    /// <summary>
+    /// Сервис для управления файловой системой
+    /// </summary>
+    public interface IStorage
+    {
+        /// <summary>
+        /// Загрузить файл
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="file">Файл</param>
+        public Task<FileData> UploadFileAsync(string path, IFormFile file);
+    }
+}
---
+++ API.Infrastructure/FileSystem/Storage.cs
@@ -0,0 +1,45 @@
+﻿using API.Infrastructure.FileSystem.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.FileSystem
+{
+    public class Storage : IStorage
+    {
+        private readonly string _webRootPath;
+        public Storage(string webRootPath)
+        {
+            _webRootPath = webRootPath;
+        }
+        public async Task<FileData> UploadFileAsync(string path, IFormFile file)
+        {
+            if (file != null)
+            {
+                path = _webRootPath + $"/Files/" + path;
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path += file.FileName;
+
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                return new FileData() { Name = file.FileName, Path = path };
+            }
+            else
+            {
+                throw new Exception("Файл не найден");
+            }
+        }
+    }
+}
