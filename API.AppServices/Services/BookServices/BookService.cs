269463818bcaa686412e3f505fa46ecdee90561b
Вячеслав <Vyacheslav>
2022-09-04 18:41:01 +0300

    Реализовал метод для добавления новой книги

---
+++ API.AppServices/Services/BookServices/BookService.cs
@@ -0,0 +1,52 @@
+﻿using API.Contracts.Book;
+using API.Domain.Entities;
+using API.Infrastructure.FileSystem;
+using API.Infrastructure.FileSystem.Models;
+using API.Infrastructure.Repository;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.AppServices.Services.BookServices
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> _bookRepository;
+        private readonly IMapper _mapper;
+        private readonly IStorage _storage;
+
+        public BookService(IRepository<Book> bookRepository, 
+            IMapper mapper, 
+            IStorage storage)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+            _storage = storage;
+        }
+
+        public async Task AddAsync(BookCreateDto model)
+        {
+            Book book = _mapper.Map<Book>(model);
+            book.Images = new List<MyFile>();
+
+            foreach (var i in model.Images)
+            {
+                FileData file = await _storage.UploadFileAsync($"/Portfolio/{book.Name}/", i);
+                book.Images.Add(new MyFile()
+                {
+                    CreationDate = DateTime.UtcNow,
+                    UpdateDate = DateTime.UtcNow,
+                    isDelete = false,
+                    FileName = file.Name,
+                    Path = file.Path
+                });
+            }
+
+
+            await _bookRepository.AddAsync(book);
+        }
+    }
+}
---
+++ API.AppServices/Services/BookServices/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using API.Contracts.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.AppServices.Services.BookServices
+{
+    public interface IBookService
+    {
+        Task AddAsync(BookCreateDto model);
+    }
+}
