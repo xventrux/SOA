f79edddc07b46dc40dfca9a7365ee17f49a6e0bb
Вячеслав <Vyacheslav>
2022-09-04 17:45:49 +0300

    Создал проект для управления миграциями

---
+++ API.DataAccess/BaseDbContext.cs
@@ -0,0 +1,27 @@
+﻿using API.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DataAccess
+{
+    /// <summary>
+    /// Базовый контекст базы данных
+    /// </summary>
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new MyFileConfiguration());
+        }
+    }
+}
---
+++ API.Migrations/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace API.Migrations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
