dcd68fa2cb5b9f1104593abae7d6a05206ee6c0b
Вячеслав <Vyacheslav>
2022-09-04 18:26:49 +0300

    Реализовал маппер

---
+++ API.Mapper/Mapping/ApplicationMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using API.Contracts.Book;
+using API.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Mapper.Mapping
+{
+    public class ApplicationMapperProfile : Profile
+    {
+        public ApplicationMapperProfile()
+        {
+            CreateMap<Book, BookCreateDto>();
+            CreateMap<BookCreateDto, Book>().ForMember(i => i.CreationDate, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(i => i.UpdateDate, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(i => i.isDelete, opt => opt.MapFrom(src => false));
+        }
+    }
+}
